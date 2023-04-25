e3d45e0cbe43faa7b32c9c33494eee6a2295d0cd
GomTang99 <106424092+GomTang99@users.noreply.github.com>
2023-04-25 15:11:23 +0900

    9 commit

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ const PORT = 5948;
 
 const app = express();
 
-// const handlelistening = () =>
-// console.log('Server listening on port http://localhost:5948');
-
-// app.listen(PORT, handlelistening);
-
 app.use(express.static(__dirname + "/C.V.C"));
+app.use('/public', express.static('public'));
+app.use('/img', express.static('img'));
 
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/html/index.html");
 });
 
-app.listen(PORT, () => {
-    console.log('Listen : ${PORT}');
+app.listen(5948, function() {
+    console.log('서버 가동');
 });
+
+// 로그인창 이동
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/html/login.html");
+});
+
+
