9d3049d992a68d817fbfc0f4ac95bf2040bb9970
jychan99 <108382087+jychan99@users.noreply.github.com>
2023-05-08 12:06:19 +0900

    reset & setup package.json
    i express

--- src/init.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const PORT = 5948;
-
-const app = express();
-
-const handleListening = () =>
-  console.log(`✅ Server listening on port http://localhost:${PORT}`);
-app.listen(PORT, handleListening);
