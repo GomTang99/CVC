a884ae4e30a3f3c376edc7a2e9e9336f69cbb512
jychan99 <108382087+jychan99@users.noreply.github.com>
2023-05-08 16:50:41 +0900

    test req,res

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ const express = require("express");
 const PORT = 5948;
 
 const app = express();
+//express application 이 만들어진 이후로 코드를 작성
 
 const handleServer = () => {
   console.log(`✅ 서버 가동 http://localhost:${PORT}`);
 };
 
+const handleMain = (req, res) => {
+  return res.send("메인화면");
+};
+
 app.listen(PORT, handleServer);
+app.get("/", handleMain);
