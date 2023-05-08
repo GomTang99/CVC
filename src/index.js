df62c50b047341fffcc00af83ead1686ef81e255
jychan99 <108382087+jychan99@users.noreply.github.com>
2023-05-08 17:26:43 +0900

    install morgan the logger middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const morgan = require("morgan"); //http메소드, url, status code등을 확인 할 수 있는 미들웨어
 const PORT = 5948;
 
 const app = express();
-//express application 이 만들어진 이후로 코드를 작성
+const logger = morgan("dev");
+//express application 이 만들어진 이후로 코드를 작성-------------------------------------------------
 
+//controller
 const handleServer = () => {
   console.log(`✅ 서버 가동 http://localhost:${PORT}`);
 };
@@ -12,5 +15,6 @@ const handleMain = (req, res) => {
   return res.send("메인화면");
 };
 
+app.use(logger); //app.use() : 모든 route에 미들웨어를 삽입
 app.listen(PORT, handleServer);
 app.get("/", handleMain);
