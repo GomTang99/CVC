ffce276e3e01662b38eb73c0b0684a05960a52a0
jychan99 <108382087+jychan99@users.noreply.github.com>
2023-05-31 22:28:33 +0900

    add formtag
    adding upload_board.ejs

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -3,6 +3,14 @@ import db from "../db.js";
 export const main = (req, res) => {
   res.render("index.ejs", { pageTitle: "index" });
 };
+
+export const getUpload = (req, res) => {
+  res.render("upload_board.ejs", { pageTitle: "Posting" });
+};
+export const postUpload = (req, res) => {
+  return res.render("/");
+};
+
 export const recruit = (req, res) => {
   res.render("recruit.ejs", { pageTitle: "recruit" });
 };
@@ -17,17 +25,17 @@ export const expertDetail = (req, res) => {
   res.render("expert.ejs", { pageTitle: "expert:detail" });
 };
 export const getFreeboard = (req, res) => {
-  const queryString = "SELECT * FROM freeboard";
+  const queryString = "SELECT * FROM freeboards";
 
   db.query(queryString, (err, rows, fields) => {
     if (err) {
       throw err;
     }
     console.log(rows);
-    const freeboard = rows;
+    const freeboards = rows;
     res.render("freeboard.ejs", {
       pageTitle: "freeboard",
-      freeboard: rows,
+      freeboards,
     });
   });
 };
@@ -35,7 +43,7 @@ export const postFreeboard = (req, res) => {
   return res.redirect("/");
 };
 export const freeboardDetail = (req, res) => {
-  res.render("freeboard.ejs", { pageTitle: "freeboard:detail" });
+  res.render("index.ejs", { pageTitle: "freeboard:detail" });
 };
 export const notice = (req, res) => {
   res.render("notice.ejs", { pageTitle: "notice" });
--- src/routers/boardRouter.js
+++ src/routers/boardRouter.js
@@ -4,6 +4,8 @@ import {
   recruitDetail,
   expert,
   expertDetail,
+  getUpload,
+  postUpload,
   getFreeboard,
   postFreeboard,
   freeboardDetail,
@@ -19,6 +21,7 @@ boardRouter.get("/recruit", recruit); //채용정보
 boardRouter.get("/recruit/:id", recruitDetail); //채용정보 상세페이지
 boardRouter.get("/expert", expert); //전문가정보
 boardRouter.get("/expert/:id", expertDetail); //전문가정보 상세페이지
+boardRouter.route("/upload").get(getUpload).post(postUpload); // 게시판 글작성
 boardRouter.route("/freeboard").get(getFreeboard).post(postFreeboard); //자유게시판
 boardRouter.get("/freeboard/:id", freeboardDetail); //자유게시판 상세페이지
 boardRouter.get("/notice", notice); //공지사항
