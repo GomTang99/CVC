9431c5f842c47d9fb182e82d3eeba48e5aee4af0
GomTang99 <106424092+GomTang99@users.noreply.github.com>
2023-08-28 04:51:12 +0900

    2023/08/28 회원가입페이지 아이디중복확인 버튼클릭시 알림창 완료 (문제점 : 가입하기버튼을눌렀을때 응답을안함)

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -58,41 +58,66 @@ globalRouter.get("/register",(req, res) => {
     res.sendFile(process.cwd() + "/html/register.html");
 });
 
-// 회원가입 구현
-globalRouter.post("/register",(req, res) => {
+// 서버 측 Node.js 코드
+globalRouter.get("/checkId", (req, res) => {
+    const id = req.query.id;
+    const checkIdsql = "SELECT users_id FROM cvc.users WHERE users_id = ?";
+    const checkIdvalue = [id];
+    
+    conn.query(checkIdsql, checkIdvalue, (err, result) => {
+        if (err) {
+            console.error('아이디 중복 체크 오류 : ', err);
+            res.status(500).json({ error: '아이디 중복 체크 오류' });
+        } else {
+            const isDuplicate = result.length > 0;
+            res.status(200).json({ isDuplicate });
+        }
+    });
+});
+
+// 회원 가입
+globalRouter.post("/register", (req, res) => {
     const id = req.body.user_id;
     const pw = req.body.user_pw;
     const name = req.body.user_name;
+    const phone = req.body.user_phone;
 
-        const sql = "INSERT INTO cvc.users (users_id, users_pw, users_name) VALUES (?, ?, ?)";
-        const value = [id, pw, name];
-        const checkIdsql = "SELECT users_id FROM cvc.users WHERE users_id = ?";
-        const checkIdvalue = [id];
-        
-        conn.query(checkIdsql, checkIdvalue, (err, result) => {
-            if (err) {
-                console.error('아이디 중복 체크 오류 : ', err);
+    const sql = "INSERT INTO cvc.users (users_id, users_pw, users_name, users_phone) VALUES (?, ?, ?, ?)";
+    const values = [id, pw, name, phone];
+
+    // 아이디 중복 체크 수행
+    const checkIdsql = "SELECT users_id FROM cvc.users WHERE users_id = ?";
+    const checkIdvalue = [id];
+
+    conn.query(checkIdsql, checkIdvalue, (err, result) => {
+        if (err) {
+            console.error('아이디 중복 체크 오류 : ', err);
+            res.status(500).json({ error: '아이디 중복 체크 오류' });
+        } else {
+            const isDuplicate = result.length > 0;
+            if (isDuplicate) {
+                //res.status(400).json({ error: '이미 존재하는 아이디입니다.' });
             } else {
-                if (result.length > 0) {
-                    console.log('이미 존재하는 아이디입니다.');
-                    res.sendFile(process.cwd() + '/html/index.html');
-                } else {
-                    // 아이디 중복이없는 경우 데이터 삽입 SQL 발생
-                    conn.query(sql, value, (err, result) => {
-                        if (err) {
-                            console.log('데이터 삽입 오류 : ', err);
-                            res.sendFile(process.cwd() + '/html/register.html')
-                        } else {
-                            console.log('데이터 삽입 성공');
-                            res.sendFile(process.cwd() + '/html/login.html');
-                        }
-                    });
-                }
+                if(id && pw && name && phone) {
+                // 아이디 중복이 없는 경우 회원 가입 처리
+                conn.query(sql, values, (err, result) => {
+                    if (err) {
+                        console.log('데이터 삽입 오류 : ', err);
+                        res.status(500).json({ error: '회원 가입 오류' });
+                    } else {
+                        console.log('데이터 삽입 성공');
+                        res.sendFile(process.cwd() + '/html/login.html');
+                        //res.status(200).json({ success: true });
+                    }
+                });
+              }
             }
-        });
-
-    //res.sendFile(process.cwd() + "/html/index.html");
+        }
+    });
 });
+
+
+
 
 globalRouter.get("/find_pw", (req, res) => {
     res.sendFile(process.cwd() + "/html/find_pw.html");
